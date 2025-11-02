47da9dfc0bf002ad7407430ae183f401004298ac
Denis Grushkin <denisoed@gmail.com>
2025-11-02 20:57:50 +0600

    chore: disable double tap zoom

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/tailwind.css']
+  css: ['~/assets/css/tailwind.css'],
+  app: {
+    head: {
+      viewport:
+        'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'
+    }
+  }
 })
